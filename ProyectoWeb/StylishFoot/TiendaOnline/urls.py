617c8d090f325e80d3e57df8ba7a856dbc9d22a4
NicolasAlvaradoo <101410957+NicolasAlvaradoo@users.noreply.github.com>
2022-05-28 20:27:52 -0400

    Cambios parte 2 python

--- ProyectoWeb/StylishFoot/TiendaOnline/urls.py
+++ ProyectoWeb/StylishFoot/TiendaOnline/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import inicio
+from .views import inicio,inicio1,registrar
 
 urlpatterns = [
     path('',inicio,name="inicio"),
+    path('inicio',inicio1,name="inicio logo"),
+    path('register',registrar,name="registrar"),
 ]
--- ProyectoWeb/StylishFoot/TiendaOnline/views.py
+++ ProyectoWeb/StylishFoot/TiendaOnline/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request,'Menú.html')
+
+def inicio1(request):
+    return render(request,'Menú.html')
+
+def registrar(request):
+    return render(request,'Register.html')
